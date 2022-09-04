b8ee6b561c7dbbdb4781c4cc8f03ec348db9685b
Jasir K <jasir.k@factset.com>
2022-09-04 23:12:51 +0530

    feat(pwa): add pwa

--- src/main.js
+++ src/main.js
@@ -5,9 +5,12 @@ import "@fortawesome/fontawesome-free/css/all.css";
 import "@fortawesome/fontawesome-free/js/all.js";
 // import { CursorFx } from "@luxdamore/vue-cursor-fx";
 import "@luxdamore/vue-cursor-fx/dist/CursorFx.css";
+
 // Vue.component(CursorFx.name, CursorFx);
 
 import jQuery from "jquery";
+
+import './registerServiceWorker'
 window.$ = jQuery;
 
 // Vue.config.productionTip = false;
